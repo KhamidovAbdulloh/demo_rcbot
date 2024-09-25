03609c25e51897b52652b90c33a6344ea7d2884a
KhamidovAbdulloh <134302308+KhamidovAbdulloh@users.noreply.github.com>
2024-09-25 17:06:57 +0500

    ew

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useTelegram } from '../../hooks/useTelegram'
-import './Header.css';
+import './Form.css';
 
-const Header = () => {
+const Form = () => {
     const {user, onClose} = useTelegram();
     <button onClick ={onClose}>Yopish</button>
     return (
         <div className={'header'}>
+            FORM
             <Button>Yopish</Button>
             <span className={'username'}> {user?.username} </span>
 
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useTelegram } from '../../hooks/useTelegram'
-import './Header.css';
+import './Product.css';
 
-const Header = () => {
+const Product = () => {
     const {user, onClose} = useTelegram();
     <button onClick ={onClose}>Yopish</button>
     return (
         <div className={'header'}>
+            PRODUCT
             <Button>Yopish</Button>
             <span className={'username'}> {user?.username} </span>
 
